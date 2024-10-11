044edf47861fb4e2c74e0ea07be5d7b2536b95b8
Julian Toledo <84879981+juliantoledo94@users.noreply.github.com>
2024-10-11 10:37:15 -0300

    feat: add first validation by a middleware

--- index.js
+++ index.js
@@ -9,15 +9,30 @@ dotenv.config()
 const app = express();
 const port = 3000;
 
-//FALTA 2 FILTROS
+
+
+
 //VALIDACIONES
 //USUARIOS JSONWEBTOKEN
+
 
 
 mongoose.connect(process.env.MONGODB_URI)
 .then(()=> console.log("conexion exitosa con mongodb!"))
 .catch((err) => console.error("error al conectar con mongodb!",err))
 app.use(express.json());
+
+
+function validateBody(req, res,next){
+    if(req.method === "POST" || req.method === "PUT" ){
+        if(Object.keys(req.body).length === 0){
+            return res.status(400).json({mensaje: "el cuerpo de la solicitud no puede estar vacio."})
+        }
+    }
+    next()
+}
+
+app.use(validateBody)
 
 app.use("/juegos",juegosRouters)
 app.use("/protagonistas",protagonistasRouters)
